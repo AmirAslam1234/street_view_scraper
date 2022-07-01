5ca1d9e4dd6b08f0bea1486ec631a943c8bdf75a
AliAhmadAx <ali.ali.ahmad22@gmail.com>
2022-07-01 17:18:42 +0500

    buttons re-design

--- src/Components/ReadOnlyRow.js
+++ src/Components/ReadOnlyRow.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ReactPaginate from "react-paginate";
 import { Data } from "./PlansData";
+import { AiFillDelete, AiFillEdit } from "react-icons/ai";
 
 function ReadOnlyRow({
   pick,
@@ -34,16 +35,16 @@ function ReadOnlyRow({
               handlePop();
             }}
             type="button"
-            className="px-3 py-2 mx-2 bg-emerald-500 text-xs text-white"
+            className="px-2 py-2 mx-2 rounded-full border text-emerald-500 hover:text-emerald-400 text-lg border-white hover:border-emerald-400"
           >
-            Edit
+            <AiFillEdit />
           </button>
           <button
             onClick={() => handleDeleteClick(pick.id)}
             type="button"
-            className="px-3 py-2 mx-2 bg-red-600 text-xs text-white"
+            className="px-2 py-2 mx-2 rounded-full border text-red-600 hover:text-red-400 text-lg border-white hover:border-red-400"
           >
-            Delete
+            <AiFillDelete className="self-center" />
           </button>
         </td>
       </tr>
